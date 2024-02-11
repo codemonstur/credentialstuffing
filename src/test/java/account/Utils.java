8b9d1b457b0501e415437df55b4fd55372b6271a
Jurgen Voorneveld <jegvoorneveld@gmail.com>
2024-02-11 16:03:33 +0100

    Updated license, updated versions

--- src/test/java/account/Utils.java
+++ src/test/java/account/Utils.java
@@ -1,11 +1,13 @@
 package account;
 
 import com.google.gson.Gson;
+import httpclient.HeaderStrategy;
 import httpclient.HttpCallRequest;
 import httpclient.Serializers;
 
 import java.net.http.HttpClient;
 
+import static httpclient.HeaderStrategy.THROW_EXCEPTION_ON_NULL;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public enum Utils {;
@@ -16,7 +18,7 @@ public enum Utils {;
     public static HttpCallRequest newHttpCall() {
         return new HttpCallRequest(HTTP_CLIENT, new Serializers.Builder()
             .json(Utils::fromJson)
-            .build());
+            .build(), THROW_EXCEPTION_ON_NULL);
     }
 
     private static <U> U fromJson(final byte[] data, final Class<U> clazz) {
